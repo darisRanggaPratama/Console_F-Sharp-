3cffedf658167acf5e643b86a6e621a02566abec
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-02-16 14:13:54 +0700

    initial commit

---
+++ dasarFsharp/basicFsharp/Program.fs
@@ -0,0 +1,7 @@
+﻿let random = System.Random()
+
+let array = Array.init 10 (fun _ -> random.Next(1001))
+
+let result = array |> Seq.filter (fun x -> x < 500) |> Seq.toArray
+
+printfn "Result: %A" result
